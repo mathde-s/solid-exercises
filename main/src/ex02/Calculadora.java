922a2711e874a3cf2089db75d435d9e567515ee1
matheus <matheus.pereira@zup.com.br>
2025-01-07 19:51:30 -0300

    refactor: implementação da classe calculadora

--- main/src/ex02/Calculadora.java
+++ main/src/ex02/Calculadora.java
@@ -1,5 +1,13 @@
 package ex02;
 
 public class Calculadora {
+    private Operacao operacao;
 
+    public void setOperacao(Operacao operacao) {
+        this.operacao = operacao;
+    }
+
+    public double calcular(int a, int b) {
+        return operacao.calcular(a, b);
+    }
 }
