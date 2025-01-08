2db83f15c3b8635ea170fed2a5f28fed0d5268fb
matheus <matheus.pereira@zup.com.br>
2025-01-08 11:37:57 -0300

    feat: criaiação e implementação da classe desenvolvedor

---
+++ main/src/ex04/Desenvolvedor/DesenvolvedorImpl.java
@@ -0,0 +1,8 @@
+package ex04.Desenvolvedor;
+
+public class DesenvolvedorImpl implements  Desenvolvedor{
+    @Override
+    public void programar() {
+        System.out.println("Programando...");
+    }
+}
