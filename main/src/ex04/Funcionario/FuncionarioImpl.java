98dc9800b067a86978105e845b12476065ad0ee2
matheus <matheus.pereira@zup.com.br>
2025-01-08 11:36:22 -0300

    criafeat: criação da interface Funcionario

---
+++ main/src/ex04/Funcionario/FuncionarioImpl.java
@@ -0,0 +1,8 @@
+package ex04.Funcionario;
+
+public class FuncionarioImpl implements Funcionario{
+    @Override
+    public void atenderCliente() {
+        System.out.println("Atendendo cliente...");
+    }
+}
